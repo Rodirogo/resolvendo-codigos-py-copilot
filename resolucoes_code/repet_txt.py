ab11dc64008b1d297f4cf027759af212b004d66c
Rodirogo <132518511+Rodirogo@users.noreply.github.com>
2026-07-22 16:43:59 -0300

    Update repet_txt.py

--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,8 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+numero1 = int(input("Digite o primeiro número: ))
+numero2 = int(input("Digite o segundo número: ))
+
+subtracao = numero1 - numero2 
+
+print("Resultado da subtração:", subtracao)
